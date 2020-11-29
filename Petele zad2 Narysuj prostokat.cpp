b45631b6be55ac3383aa20a7022d79ab03f06d93
Lucas <75207918+Ringthebells@users.noreply.github.com>
2020-11-29 15:08:24 +0100

    Add files via upload
    Program rysujący prostokąt o podanych wymiarach.

---
+++ Petele zad2 Narysuj prostokat.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+#include<locale.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "Polish");
+	int a;
+	int b;
+	cout << "Podaj szerokość prostokąta ";
+	cin >> a;
+	cout << "Podaj wysokość prostokąta ";
+	cin >> b;
+	if ((a < 2) || (b < 2))
+	{
+		cout << "Podano złe wartości boków (minimalna wartość boku rówana 2)";
+	}
+	else
+	{
+		for (int i = a; i >= 1; i--)
+		{
+			cout << "x";
+		}
+		cout << endl;
+		for (int i = b - 2; i >= 1; i--)
+		{
+			cout << "x";
+			for (int i = a - 2; i >= 1; i--)
+			{
+				cout << " ";
+			}
+			cout << "x";
+			cout << endl;
+		}
+		for (int i = a; i >= 1; i--)
+		{
+			cout << "x";
+		}
+	}
+	return 0;
+}
